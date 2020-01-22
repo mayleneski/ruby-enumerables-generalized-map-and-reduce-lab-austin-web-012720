c1c63a3855232de20550e4b314c5fdf11b9a51a6
Maylene Poulsen <mayleneski@hotmail.com>
2020-01-22 21:09:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,14 @@
+require 'pry'
 def map(array)
-  new_array = []
-  i = 0
-
-  while i < array.length
-    new_array.push(yield(array[i]))
+new_array = []
+i = 0
+  while i < array.length do
+    new_array << yield(array[i])
   i += 1
-  end
+  end 
   new_array
 end  
+ 
 
 
 def reduce(array, starting_value = nil)
